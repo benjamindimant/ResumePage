d6d6d7eedc9a13649cfb1a7cca937047aaa9415b
benjamindimant <benjamin@dimant.com>
2017-09-12 17:26:24 +0100

    Add content to about section

--- navbar.js
+++ navbar.js
@@ -1,15 +1,27 @@
-var a = $(".navbar").offset().top;
+$(document).scroll(function() {
+  if ($(this).scrollTop() >= $("#projects").offset().top - 40) {
+    $('#navbarCollapse > ul > li.nav-item > a').css({"color": "#222"});
+  } else if ($(this).scrollTop() >= $("#experience").offset().top - 40) {
+    $('#navbarCollapse > ul > li.nav-item > a').css({"color": "#f8f9fa"});
+  } else if ($(this).scrollTop() >= $("#education").offset().top - 40) {
+    $('#navbarCollapse > ul > li.nav-item > a').css({"color": "#222"});
+  } else if ($(this).scrollTop() >= $("#about").offset().top - 40) {
+    $('#navbarCollapse > ul > li.nav-item > a').css({"color": "#f8f9fa"});
+  } else {  
+    $('#navbarCollapse > ul > li.nav-item > a').css({"color": "#222"});
+  }
+});
 
 $(document).scroll(function() {
-  if ($(this).scrollTop() >= $("#projects").offset().top) {
-    $('.navbar').css({"background-color": "#f8f9fa", "box-shadow": "0 0 100px 1px #f8f9fa"});
-  } else if ($(this).scrollTop() >= $("#experience").offset().top) {
-    $('.navbar').css({"background-color": "#222", "box-shadow": "0 0 100px 1px #222", "color": "#f8f9fa"});
-  } else if ($(this).scrollTop() >= $("#education").offset().top) {
-    $('.navbar').css({"background-color": "#f8f9fa", "box-shadow": "0 0 100px 1px #f8f9fa"});
-  } else if ($(this).scrollTop() >= $("#about").offset().top) {
-    $('.navbar').css({"background-color": "#222", "box-shadow": "0 0 100px 1px #222", "color": "#f8f9fa"});
+  if ($(this).scrollTop() >= $("#projects").offset().top - 40) {
+    $('.navbar-brand').css({"color": "#222"});
+  } else if ($(this).scrollTop() >= $("#experience").offset().top - 40) {
+    $('.navbar-brand').css({"color": "#f8f9fa"});
+  } else if ($(this).scrollTop() >= $("#education").offset().top - 40) {
+    $('.navbar-brand').css({"color": "#222"});
+  } else if ($(this).scrollTop() >= $("#about").offset().top - 40) {
+    $('.navbar-brand').css({"color": "#f8f9fa"});
   } else {  
-    $('.navbar').css({"background-color": "#f8f9fa", "box-shadow": "0 0 100px 1px #f8f9fa"});
+    $('.navbar-brand').css({"color": "#222"});
   }
 });
